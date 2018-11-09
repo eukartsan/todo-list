0440c29eb9894708ef2c3772148e96d620ab52a8
eukartsan <eugenekartsan@gmail.com>
2018-11-09 12:50:17 +0200

    change todoform

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,30 +3,32 @@ import './Main.css';
 import TodoList from '../TodoList/TodoList';
 import TodoForm from '../TodoForm/TodoForm';
 
-const TodoBanner = () => <h3 className='todo__banner'>
-  TODO List < /h3>; export default class Main extends React.Component {
-    constructor() {
-      super()
+const TodoBanner = () => <h3 className='todo__banner'>TODO List</h3>;
 
-      this.state = {
-        items: ['Наладить добавление новых задач в TODO List', 'Предотвратить добавление пустых элементов', 'Перенести фокус на поле ввода новой задачи при старте приложения. И переносить фокус туда же при добавлении каждой новой задачи в список']
-      }
+export default class Main extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      items: ['Наладить добавление новых задач в TODO List', 'Предотвратить добавление пустых элементов', 'Перенести фокус на поле ввода новой задачи при старте приложения. И переносить фокус туда же при добавлении каждой новой задачи в список']
     }
+  }
 
-    addNewItems = (itemName) => {
-      const {items} = this.state
+  addNewItems = (itemName) => {
+    const {items} = this.state
 
-      if ((itemName) !== '') {
-        this.setState({items: items.concat(itemName)});
-      }
+    if ((itemName) !== '') {
+      this.setState({items: items.concat(itemName)});
     }
+  }
 
-    render() {
-      return (<div className='todo'>
-        <TodoBanner >
-          <div className="App">
-            <TodoList items={this.state.items}/>
-            <TodoForm="TodoForm" addItems={this.addNewItems}></div>
-          </div>);
-}
+  render() {
+    return (<div className='todo'>
+      <TodoBanner/>
+      <div className="App">
+        <TodoList items={this.state.items}/>
+        <TodoForm addItems={this.addNewItems}/>
+      </div>
+    </div>);
+  }
 }
